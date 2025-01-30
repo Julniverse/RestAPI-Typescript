33419e7223563261525ab9f4f7e7a33b022f7b74
Julniverse <juliandikurniansyah@gmail.com>
2025-01-30 21:14:12 +0700

    Day 9

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,23 +1,25 @@
 import { Request, Response } from 'express'
 import { createProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { getProductFromDB } from '../services/product.service'
+import { addProductToDB, getProductFromDB } from '../services/product.service'
+import { v4 as uuidv4 } from 'uuid'
+import ProductType from '../types/product.type'
 
-interface ProductType {
-  product_id: String
-  name: String
-  price: Number
-  size: String
-}
-
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+  req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
   if (error) {
     logger.error('ERR: product - create = ', error.details[0].message)
     return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
   }
-  logger.info('Success add new product')
-  return res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
+  try {
+    await addProductToDB(value)
+    logger.info('Success add new product')
+    return res.status(200).send({ status: true, statusCode: 200, message: 'Add product success' })
+  } catch (error) {
+    logger.error('ERR: product - create = ', error)
+    return res.status(422).send({ status: false, statusCode: 422, message: error })
+  }
 }
 
 export const getProduct = async (req: Request, res: Response) => {
